29e652526e4c7b3d17b299791671c02cd310b8cb
athira <athiraomanakuttan20@gmail.com>
2024-10-07 12:17:56 +0530

    Some error handling methods added

--- api/app.js
+++ api/app.js
@@ -4,8 +4,23 @@ import connectDB from './Configuration/dbConnection.js';
 const app = express()
 app.use(express.json())
 app.use('/api/user',userRouter)
-const PORT = process.env.PORT || 8000
 
+
+//error handling middleware
+
+app.use((err,req,res,next)=>{
+  const errorCode = err.status || 500
+  const message = err.message || "internal server error";
+  return res.status(errorCode).json({
+    success: false,
+    error: errorCode,
+    message
+  })
+})
+
+
+// port configuration
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
     connectDB()
--- api/controller/user/signupController.js
+++ api/controller/user/signupController.js
@@ -1,20 +1,17 @@
 
-import {  userCollection } from '../../models/userSchema.js';
+import { userCollection } from '../../models/userSchema.js';
 import bcrypt from 'bcryptjs'
 
-export const userSignup = async (req,res)=>{
-    const { userName, userEmail, password} = req.body;
-    const salt =10;
-    const hashedPassword =await bcrypt.hash(password, salt)
-    try{
-        const newUser  = await userCollection.insertMany({userName, userEmail, password: hashedPassword})
-    console.log("data recived =========== > ",newUser)
-    res.status(200).json({message:"successfully user added"});
-
+export const userSignup = async (req, res, next) => {
+    const { userName, userEmail, password } = req.body;
+    const salt = 10;
+    const hashedPassword = await bcrypt.hash(password, salt)
+    try {
+        const newUser = await userCollection.insertMany({ userName, userEmail, password: hashedPassword })
+        res.status(200).json({ message: "User added successfully" });
     }
-    catch(err)
-    {
-        console.log("error occured while adding data",err)
-        res.status(500).json({error : err})
+    catch (err) {
+        console.log("error occured while adding data", err)
+        next(err)
     }
 }
