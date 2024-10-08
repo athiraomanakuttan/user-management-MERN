c583427633afb8d3e148a4730d372a1793708505
athira <athiraomanakuttan20@gmail.com>
2024-10-08 10:47:13 +0530

    login api created with jwt

--- api/controller/user/signupController.js
+++ api/controller/user/signupController.js
@@ -1,6 +1,9 @@
 
 import { userCollection } from '../../models/userSchema.js';
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const userSignup = async (req, res, next) => {
     const { userName, userEmail, password } = req.body;
@@ -11,7 +14,32 @@ export const userSignup = async (req, res, next) => {
         res.status(200).json({ message: "User added successfully" });
     }
     catch (err) {
-        console.log("error occured while adding data", err)
+        next(err)
+    }
+}
+export const userLogin = async (req,res,next)=>{
+    const { userEmail,password}= req.body;
+    
+    try{
+        const validUser = await userCollection.findOne({ userEmail});
+        if(!validUser)
+            next({message : "User Not Found"});
+        else
+        {
+            const validPassword = await bcrypt.compare(password, validUser.password);
+
+            if(!validPassword)
+                next({message: "Incorrect Password"}) 
+            else
+            {
+                const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET)
+                res.cookie('token',token, {httpOnly: true}).status(200).json(validUser)
+            }
+        }
+    }
+    catch(err)
+    {
+        console.log(err)
         next(err)
     }
 }
